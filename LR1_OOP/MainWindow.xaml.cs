fc630b38e2297345c5de7cc5fab657cd96d44534
Павел Мискевич <chiki-takipavel@mail.ru>
2020-03-09 18:47:19 +0300

    Добавлена возможность рисования с помощью мыши

--- LR1_OOP/MainWindow.xaml.cs
+++ LR1_OOP/MainWindow.xaml.cs
@@ -11,10 +11,33 @@ namespace LR1_OOP
     /// </summary>
     public partial class MainWindow : Window
     {
-        SolidColorBrush brushStroke;
-        SolidColorBrush brushFill;
-        double widthStroke;
-        NewShapeList list;
+        private SolidColorBrush brushStroke;
+        private SolidColorBrush brushFill;
+        private double widthStroke;
+        private PointCollection pointsList;
+        //private bool flagDraw;
+        private int countPoints;
+        private NewShapeList list;
+
+        public int CountPoints
+        {
+            get { return countPoints; }
+            set
+            {
+                if (value < 2)
+                {
+                    countPoints = 2;
+                }
+                else if (value > 20)
+                {
+                    countPoints = 20;
+                }
+                else
+                {
+                    countPoints = value;
+                }
+            }
+        }
 
         public MainWindow()
         {
@@ -22,17 +45,23 @@ namespace LR1_OOP
             brushStroke = new SolidColorBrush(Colors.Black);
             brushFill = new SolidColorBrush(Colors.White);
             widthStroke = 7;
+            pointsList = new PointCollection();
+            //flagDraw = false;
+            CountPoints = 2;
+            txtCountPoints.DataContext = this;
 
             ObservableCollection<string> comboItems = new ObservableCollection<string>();
             cmbShapes.ItemsSource = comboItems;
             comboItems.Add("Линия");
             comboItems.Add("Прямоугольник");
             comboItems.Add("Эллипс");
+            comboItems.Add("Полигон");
 
             list = new NewShapeList();
-            list.Shapes.Add(new NewLine(widthStroke, brushStroke, brushFill, new Point(56, 345), new Point(467, 475)));
-            list.Shapes.Add(new NewRectangle(widthStroke, brushStroke, brushFill, new Point(10, 10), new Point(200, 200)));
-            list.Shapes.Add(new NewEllipse(widthStroke, brushStroke, brushFill, new Point(400, 400), new Point(600, 600)));
+            list.Shapes.Add(new NewLine(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewRectangle(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewEllipse(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewEllipse(widthStroke, brushStroke, brushFill, pointsList));
 
             slidStrWidth.Value = widthStroke;
             rectStrokeColor.Fill = brushStroke;
@@ -65,15 +94,53 @@ namespace LR1_OOP
 
         private void btnDraw_Click(object sender, RoutedEventArgs e)
         {
-            list.Shapes[cmbShapes.SelectedIndex].FillBrush = brushFill;
-            list.Shapes[cmbShapes.SelectedIndex].StrokeBrush = brushStroke;
-            list.Shapes[cmbShapes.SelectedIndex].StrokeWidth = widthStroke;
-            list.Shapes[cmbShapes.SelectedIndex].Draw(canvasField);
+            //pointsList.Clear();
+            //flagDraw = !flagDraw;
+            //if (flagDraw)
+            //{
+            //    //cmbShapes.IsEnabled = false;
+            //    btnDraw.Content = "Отмена";
+            //    System.Windows.MessageBox.Show("Выберите точки на экране", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //else
+            //{
+            //    //cmbShapes.IsEnabled = true;
+            //    btnDraw.Content = "Нарисовать";
+            //}
+        }
+
+        private void canvasField_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //if (flagDraw)
+            pointsList.Add(e.GetPosition(canvasField));
+            if (pointsList.Count == CountPoints)
+            {
+                list.Shapes[cmbShapes.SelectedIndex].StrokeBrush = brushStroke;
+                list.Shapes[cmbShapes.SelectedIndex].FillBrush = brushFill;
+                list.Shapes[cmbShapes.SelectedIndex].StrokeWidth = widthStroke;
+                list.Shapes[cmbShapes.SelectedIndex].Points = pointsList;
+                list.Shapes[cmbShapes.SelectedIndex].Draw(canvasField);
+                pointsList.Clear();
+            }
         }
 
         private void slidStrWidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             widthStroke = slidStrWidth.Value;
+        }
+
+        private void cmbShapes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            pointsList.Clear();
+            CountPoints = 2;
+            if (cmbShapes.SelectedIndex == 3)
+            {
+                panelCountPoints.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                panelCountPoints.Visibility = Visibility.Hidden;
+            }
         }
 
         private void itemExit_Click(object sender, RoutedEventArgs e)
