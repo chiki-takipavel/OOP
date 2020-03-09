d3e75554c747dd44ff20e6633f3ec4de12539648
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-03-10 01:15:27 +0300

    Добавлена проверка на ввод количества точек в "Многоугольнике"

---
+++ LR1_OOP/RangeValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace LR1_OOP
+{
+    public class RangeValidationRule : ValidationRule
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+
+        public override ValidationResult Validate(
+          object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            int intValue;
+
+            string text = String.Format("Должно быть между {0} и {1}",
+                           MinValue, MaxValue);
+            if (!Int32.TryParse(value.ToString(), out intValue))
+                return new ValidationResult(false, "Не целое число");
+            if (intValue < MinValue)
+                return new ValidationResult(false, "Слишком маленькое. " + text);
+            if (intValue > MaxValue)
+                return new ValidationResult(false, "Слишком большое. " + text);
+            return ValidationResult.ValidResult;
+        }
+    }
+}
