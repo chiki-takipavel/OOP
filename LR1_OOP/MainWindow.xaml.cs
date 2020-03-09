eda95dc6025faf88b6c8d42de092c26f3f7e3026
Pavel Miskevich <chiki-takipavel@users.noreply.github.com>
2020-03-09 15:21:13 +0300

    Добавлен полигон и рисование с помощью мыши

--- LR1_OOP/MainWindow.xaml.cs
+++ LR1_OOP/MainWindow.xaml.cs
@@ -11,32 +11,61 @@ namespace LR1_OOP
     /// </summary>
     public partial class MainWindow : Window
     {
-        Color defaultStrColor;
-        Color defaultFillColor;
-        double defaultStrWidth;
-        NewShapeList list;
+        private SolidColorBrush brushStroke;
+        private SolidColorBrush brushFill;
+        private double widthStroke;
+        private PointCollection pointsList;
+        //private bool flagDraw;
+        private int countPoints;
+        private NewShapeList list;
+
+        public int CountPoints
+        {
+            get { return countPoints; }
+            set
+            {
+                if (value < 2)
+                {
+                    countPoints = 2;
+                }
+                else if (value > 20)
+                {
+                    countPoints = 20;
+                }
+                else
+                {
+                    countPoints = value;
+                }
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
-            defaultStrColor = Colors.Black;
-            defaultFillColor = Colors.White;
-            defaultStrWidth = 7;
+            brushStroke = new SolidColorBrush(Colors.Black);
+            brushFill = new SolidColorBrush(Colors.White);
+            widthStroke = 7;
+            pointsList = new PointCollection();
+            //flagDraw = false;
+            CountPoints = 2;
+            txtCountPoints.DataContext = this;
 
             ObservableCollection<string> comboItems = new ObservableCollection<string>();
             cmbShapes.ItemsSource = comboItems;
             comboItems.Add("Линия");
             comboItems.Add("Прямоугольник");
             comboItems.Add("Эллипс");
+            comboItems.Add("Полигон");
 
             list = new NewShapeList();
-            list.Shapes.Add(new NewLine(defaultStrWidth, defaultStrColor, defaultFillColor, new Point(56, 345), new Point(467, 475)));
-            list.Shapes.Add(new NewRectangle(defaultStrWidth, defaultStrColor, defaultFillColor, new Point(10, 10), new Point(200, 200)));
-            list.Shapes.Add(new NewEllipse(defaultStrWidth, defaultStrColor, defaultFillColor, new Point(400, 400), new Point(600, 600)));
+            list.Shapes.Add(new NewLine(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewRectangle(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewEllipse(widthStroke, brushStroke, brushFill, pointsList));
+            list.Shapes.Add(new NewEllipse(widthStroke, brushStroke, brushFill, pointsList));
 
-            slidStrWidth.Value = defaultStrWidth;
-            rectStrokeColor.Fill = new SolidColorBrush(defaultStrColor);
-            rectFillColor.Fill = new SolidColorBrush(defaultFillColor);
+            slidStrWidth.Value = widthStroke;
+            rectStrokeColor.Fill = brushStroke;
+            rectFillColor.Fill = brushFill;
         }
 
         private void btnStrokeColor_Click(object sender, RoutedEventArgs e)
@@ -46,7 +75,7 @@ namespace LR1_OOP
             {
                 System.Drawing.Color color = colorPicker.Color;
                 SolidColorBrush brushColor = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
-                list.Shapes[cmbShapes.SelectedIndex].StrokeBrush = brushColor;
+                brushStroke = brushColor;
                 rectStrokeColor.Fill = brushColor;
             }
         }
@@ -58,19 +87,60 @@ namespace LR1_OOP
             {
                 System.Drawing.Color color = colorPicker.Color;
                 SolidColorBrush brushColor = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
-                list.Shapes[cmbShapes.SelectedIndex].FillBrush = brushColor;
+                brushFill = brushColor;
                 rectFillColor.Fill = brushColor;
             }
         }
 
         private void btnDraw_Click(object sender, RoutedEventArgs e)
         {
-            list.Shapes[cmbShapes.SelectedIndex].Draw(canvasField);
+            //pointsList.Clear();
+            //flagDraw = !flagDraw;
+            //if (flagDraw)
+            //{
+            //    //cmbShapes.IsEnabled = false;
+            //    btnDraw.Content = "Отмена";
+            //    System.Windows.MessageBox.Show("Выберите точки на экране", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //else
+            //{
+            //    //cmbShapes.IsEnabled = true;
+            //    btnDraw.Content = "Нарисовать";
+            //}
+        }
+
+        private void canvasField_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //if (flagDraw)
+            pointsList.Add(e.GetPosition(canvasField));
+            if (pointsList.Count == CountPoints)
+            {
+                list.Shapes[cmbShapes.SelectedIndex].StrokeBrush = brushStroke;
+                list.Shapes[cmbShapes.SelectedIndex].FillBrush = brushFill;
+                list.Shapes[cmbShapes.SelectedIndex].StrokeWidth = widthStroke;
+                list.Shapes[cmbShapes.SelectedIndex].Points = pointsList;
+                list.Shapes[cmbShapes.SelectedIndex].Draw(canvasField);
+                pointsList.Clear();
+            }
         }
 
         private void slidStrWidth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            list.Shapes[cmbShapes.SelectedIndex].StrokeWidth = slidStrWidth.Value;
+            widthStroke = slidStrWidth.Value;
+        }
+
+        private void cmbShapes_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            pointsList.Clear();
+            CountPoints = 2;
+            if (cmbShapes.SelectedIndex == 3)
+            {
+                panelCountPoints.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                panelCountPoints.Visibility = Visibility.Hidden;
+            }
         }
 
         private void itemExit_Click(object sender, RoutedEventArgs e)
